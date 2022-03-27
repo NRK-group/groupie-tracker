185c1cf16626d4680549617bd61e98650683e9a4
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-03-27 14:48:04 +0100

    Add handler for the static folder

--- main.go
+++ main.go
@@ -9,8 +9,9 @@ func main() {
 	// The "HandleFunc" method accepts a path and a handler function as arguments
 	// The handler function has to have the appropriate signature (as described by the "handler" function below)
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/artists/", getArtistHandler)
-
+	http.HandleFunc("/artists", getArtistHandler)                  // this code handle the all the file in the static folder
+	cssPath := http.FileServer(http.Dir("./static"))               // this code handle the all the file in the static folder
+	http.Handle("/static/", http.StripPrefix("/static/", cssPath)) // handling the CSS
 	// After defining the server, we "listen and serve" on port 8080
 	// The second argument is the handler
 	http.ListenAndServe(":8000", nil)
