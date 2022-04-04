1fd79856d5e0fa5221bcc8ab6ff7f67c6a4c8c88
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-04-04 17:07:55 +0100

    Fix the conflicts

--- artist_handlers.go
+++ artist_handlers.go
@@ -13,46 +13,51 @@ type Data struct {
 	Index interface{}
 }
 
-func getArtistHandler(w http.ResponseWriter, r *http.Request) {
-	// Get response to the API
-
-	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+//GetAPI receive an api url and return the api into []byte.
+//This function will get the url that you give, read it and return it as a []byte
+func GetAPI(url string) []byte {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer resp.Body.Close()
+	return body
+}
 
+func getArtistHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 Status not found", http.StatusNotFound)
+		return
+	}
+	url := "https://groupietrackers.herokuapp.com/api"
 	// let's declare an Articles array
 	// that we can then populate in our main function
 	// to simulate a database
 	var Artists []Artist
-	body, _ := ioutil.ReadAll(resp.Body)
-	if err := json.Unmarshal(body, &Artists); err != nil {
-		fmt.Println("ERR")
+	if err := json.Unmarshal(GetAPI(url+"/artists"), &Artists); err != nil {
+		fmt.Println("err")
 	}
 
 	//unpacking the relation api
-	resp2, err := http.Get("https://groupietrackers.herokuapp.com/api/relation")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer resp2.Body.Close()
 	var Relation Relations
-	body2, _ := ioutil.ReadAll(resp2.Body)
-	if err := json.Unmarshal(body2, &Relation); err != nil {
+	if err := json.Unmarshal(GetAPI(url+"/relation"), &Relation); err != nil {
 		fmt.Println("err")
 	}
 
 	// the code below gets the ID of the button that has been clicked and convert it to interger
-	b := r.FormValue("submit")
-	idNum, err := strconv.Atoi(b)
+	id := r.FormValue("artist")
+	idNum, err := strconv.Atoi(id)
 	if err != nil {
 		idNum = 1
 	}
 
 	// data will be all the information that will be in the inforamtion section
 	data := Artists[idNum-1]
-	data.Concerts = Relation.Index[idNum-1] // add the concert from relation api into the data
+	data.Concerts = Relation.Index[idNum-1] // add the concert from relation api into the data struct
 
 	info := map[string]interface{}{
 		"Artists": Artists,
--- main.go
+++ main.go
@@ -7,14 +7,12 @@ import (
 func main() {
 	// The "HandleFunc" method accepts a path and a handler function as arguments
 	// The handler function has to have the appropriate signature (as described by the "handler" function below)
-	// http.HandleFunc("/", handler)
-	// http.HandleFunc("/artists", getArtistHandler)
 	http.HandleFunc("/", getArtistHandler)
+	favico := http.FileServer(http.Dir("./favicon_io"))                   // handling the adress bar icon
+	http.Handle("/favicon_io/", http.StripPrefix("/favicon_io/", favico)) // handling the icon for the adress bar
 	cssPath := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", cssPath)) // handling the CSS
-	favico := http.FileServer(http.Dir("./favicon_io")) // handling the adress bar icon
-	http.Handle("/favicon_io/", http.StripPrefix("/favicon_io/", favico)) // handling the icon for the adress bar
-	// After defining the server, we "listen and serve" on port 8080
+	// After defining the server, we "listen and serve" on port 8000
 	// The second argument is the handler
 	http.ListenAndServe(":8000", nil)
 }
